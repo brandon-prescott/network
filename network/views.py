a54666ffcc07dac514bcc246cbf804421f65e023
brandon-prescott <brandon.prescott@live.com>
2023-04-29 22:40:58 +0100

    any profile can be requested

--- network/views.py
+++ network/views.py
@@ -87,15 +87,16 @@ def create(request):
         return redirect(index)
     
 
-def profile(request):
-    all_user_posts = Post.objects.filter(user=request.user.id).order_by("-time")
+def profile(request, user_id):
+    all_user_posts = Post.objects.filter(user=user_id).order_by("-time")
     number_of_posts = len(all_user_posts)
 
-    followers_count = len(Follow.objects.filter(following=request.user.id))
-    following_count = len(Follow.objects.filter(user=request.user.id))
+    followers_count = len(Follow.objects.filter(following=user_id))
+    following_count = len(Follow.objects.filter(user=user_id))
     
     return render(request, "network/profile.html", {
         "user": User.objects.get(id=request.user.id),
+        "profile": User.objects.get(id=user_id),
         "followers_count": followers_count,
         "following_count": following_count,
         "all_user_posts": all_user_posts,
