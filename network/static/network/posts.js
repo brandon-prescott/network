9970e38281be1f6b4ebd2eff93ca712d50af0de5
brandon-prescott <brandon.prescott@live.com>
2023-05-05 12:06:06 +0100

    click events for edit btns

--- network/static/network/posts.js
+++ network/static/network/posts.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     console.log("DOM Content Loaded!");
-    console.log("Wassup!")
+
+    // Select all buttons with IDs in the format "edit-btn-{id}"
+    const editBtns = document.querySelectorAll('[id^="edit-btn-"]');
+
+    // Loop through each button and listen for click
+    editBtns.forEach(function(editBtn) {
+        editBtn.addEventListener('click', function() {
+            // Get ID of the current post
+            const postID = editBtn.id.replace('edit-btn-', '');
+
+            // Do stuff with the click
+            console.log(`Edit Button ${postID} clicked!`);
+
+        });
+    });
 
 })
+
